b93fe134d677bd53d802346afb3f6b088bd4ca5d
Himanshu mishra <164254902+HimanshuMishra123@users.noreply.github.com>
2024-06-22 00:21:56 +0530

    ...

--- day-25/lambda_function.py
+++ day-25/lambda_function.py
@@ -1,22 +1,32 @@
 import boto3
 import json
 
-def lambda_handler(event, context):
+#lambda_handler function is triggered by aws config
+#event parameter contains data(JSON) that triggered the labda function (Example - if your function is triggered by a change in an EC2 instance configuration, event will include information about that change)
+#context parameter contains information about the Lambda function’s execution environment. Example - Request ID , Function name etc.
+def lambda_handler(event, context):          
 
     # Get the specific EC2 instance.
     ec2_client = boto3.client('ec2')
     
+
     # Assume compliant by default
     compliance_status = "COMPLIANT"  
     
-    # Extract the configuration item from the invokingEvent
-    config = json.loads(event['invokingEvent'])
-    
+
+    #example of "event" data  ....
+    #event = {
+    #"invokingEvent": "{\"configurationItem\": {\"configuration\": {\"instanceId\": \"i-0123456789abcdef0\"}}, \"notificationCreationTime\": \"2024-06-21T12:00:00Z\"}",
+    #"resultToken": "example-token"
+    # }
+    # Extract the invoking event item from the event data and parsing the Json to dict.
+    config = json.loads(event['invokingEvent'])   
+    # Extract the configuration item 
     configuration_item = config["configurationItem"]
-    
     # Extract the instanceId
     instance_id = configuration_item['configuration']['instanceId']
     
+
     # Get complete Instance details
     instance = ec2_client.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
     
@@ -37,7 +47,9 @@ def lambda_handler(event, context):
     
     response = config_client.put_evaluations(
         Evaluations=[evaluation],
-        ResultToken=event['resultToken']
+        ResultToken=event['resultToken']    #Identifies the rule and the event that triggered the evaluation.
+
+
     )  
     
     return response
