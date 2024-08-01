e9c0820a8837002ab33ed259bc0c85140043a090
Himanshu mishra <164254902+HimanshuMishra123@users.noreply.github.com>
2024-08-02 02:42:43 +0530

    code-build

--- day-14/simple-python-app(flask framework)/start_container.sh
+++ day-14/simple-python-app(flask framework)/start_container.sh
@@ -5,4 +5,5 @@ set -e
 docker pull abhishekf5/sample-python-flask-app
 
 # Run the Docker image as a container  -p for prt mapping, -it for interactive mode, -d to run in background or as daemon.
+# In computing, a daemon (pronounced DEE-muhn) is a program that runs continuously as a background process and wakes up to handle periodic service requests, which often come from remote processes.
 docker run -d -p 5000:8000
